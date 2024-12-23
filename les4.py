cd6efc5937ed5b8561b8c9c1c2f2393518042ab2
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 19:40:30 +0500

    done 4 mashq

---
+++ les4.py
@@ -0,0 +1,22 @@
+# Birinchi misol!
+num1 = int(input("1-sonni kiriting: "))
+num2 = int(input("2-sonni kiriting: "))
+
+print("\n")
+
+Yigindi = num1 + num2
+Kupaytma = num1 * num2
+print(f"Yig'indisi: {Yigindi}")
+print(f"Kupaytmasi: {Kupaytma}")
+print("\n")
+
+# Ikkinchi misol!
+num3 = float(input("3-sonni kiriting: "))
+num4 = float(input("4-sonni kiriting: "))
+
+print("\n")
+
+FYigindi = num3 + num4
+Fkupaytma = num3 * num4
+print(f"Yig'indisi: {FYigindi}")
+print(f"Kupaytmasi: {Fkupaytma}")
