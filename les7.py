bdcf74385d8f41ad9a9e448a8225824b398289ca
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 20:02:18 +0500

    done 7 mashq

--- les7.py
+++ les7.py
@@ -1,6 +1,8 @@
+# son kiritamiz
 num1 = int(input("1-Sonni kiriting: "))
 num2 = int(input("2-Sonni kiriting: "))
 
 butun_num = num1 // num2
 
+# Javobini chiqaramiz
 print(f"Butun sonimiz: {butun_num} ")
