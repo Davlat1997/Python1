8632e1e170ebc660b82ae15dc35836c8e2dc05c2
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 18:14:47 +0500

    done 3 mashq

---
+++ les3.py
@@ -0,0 +1,5 @@
+name = input("Sizning ismiz? ")
+colour = input("Sizning sevimli rangingiz? ")
+
+print(f"Assalomu Aleykum va rahmatullohu va barokatuh - {name} " )
+print(f"sevimli rangingiz - {colour} mmm yaxshi :) sizam (Rosululloh ﷺ )yaxshi kurgan ranglarini yaxshi kurarkansiz! ")
