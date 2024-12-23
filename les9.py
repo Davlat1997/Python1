cc1324764b53133e2b1d48a43cfbe41771169f49
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 23:25:34 +0500

    done 9 mashq

---
+++ les9.py
@@ -0,0 +1,10 @@
+num1 = float(input("1-sonni kiriting: "))
+num2 = float(input("2-sonni kiriting: "))
+
+natija = num1 != num2 and num1 > num2
+
+print("Birinchi son ikkinchi sondan kattami?")
+print("\n")
+print(natija)
+print("Ikkalasi teng!" * (num1 == num2))
+    
