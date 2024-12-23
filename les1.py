8464113a0bad37f4e55fb8d1904a2d443d69a508
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 17:53:47 +0500

    done 1 mashq

---
+++ les1.py
@@ -0,0 +1,11 @@
+# Ma'lumotlat turi!
+intreger = 32
+float_value = 4.32
+string = "matn"
+booling = True or False
+
+# print orqali har bir ma'lumotni alohida chiqarish.
+print(intreger)
+print(float_value)
+print(string)
+print(booling)
