8ff5e5b7e058760222e066bfc655a50bc30f8ed7
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-24 11:18:21 +0500

    done 20 mashq

---
+++ les20.py
@@ -0,0 +1,13 @@
+name = input("Ismingizni kiriting: ")
+surname = input("Familiyangizni kiriting: ")
+
+username = name + surname
+username2 = surname + name
+username3 = name.lower() + surname.upper()
+username4 = surname.upper() + name.lower()
+
+
+print(username)
+print(username2)
+print(username3)
+print(username4)
