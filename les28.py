d9754032161171a5eab4802c06292fbc56d3b092
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-24 20:52:13 +0500

    turtle drawing

---
+++ les28.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+shape('turtle')
+
+Screen().title('turtle')
+Screen().setup(1000, 1000)
+Screen().bgcolor('black')
+color('lime')
+speed(0)
+width(10)
+
+
+forw = 15
+while True:
+    forward(forw)
+    left(80)
+    forw += 10
+
+
+
+
+
+forward(100)
+done()
