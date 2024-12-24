4d434adb21494149b01ba52ea1ab81e14f6900d4
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-24 11:12:31 +0500

    done 19 mashq

---
+++ les19.py
@@ -0,0 +1,11 @@
+name = input("Ismingizni kiriting: ")
+surname = input("Familiyangizni kiriting: ")
+
+usersname = name + surname
+usersname2 = name.lower() + ".!" + surname.lower()
+usersname3= name[0].lower() + surname.lower() + "1997"
+
+
+print(usersname)
+print(usersname2)
+print(usersname3)
